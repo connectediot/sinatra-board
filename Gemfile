f1c7a0789907a9df0dfa4ae86e2e97d4ea8e308f
ChangHee <och8808@naver.com>
2018-06-12 05:16:56 +0000

    bunlder 및 gemfile

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'datamapper'
+gem 'dm-sqlite-adapter'
+gem 'json', '~> 1.6'
--- app.rb
+++ app.rb
@@ -1,27 +1,6 @@
-gem 'json', '~> 1.6'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'data_mapper' # metagem, requires common plugins too.
-
-# datamapper 로그 찍기
-DataMapper::Logger.new($stdout, :debug)
-# need install dm-sqlite-adapter
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
-
-class Post
-  include DataMapper::Resource
-  property :id, Serial
-  property :title, String
-  property :body, Text
-  property :created_at, DateTime
-end
-
-# Perform basic sanity checks and initialize all relationships
-# Call this when you've defined all your models
-DataMapper.finalize
-
-# automatically create the post table
-Post.auto_upgrade!
+require './model.rb'
 
 before do 
   p '***************************'
